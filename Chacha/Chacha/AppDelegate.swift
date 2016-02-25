5eb7b534fdb9452f8ae383f3f9c6100d3e0aabc4
danielchangsoojones <danieljones@Daniels-MBP-3.attlocal.net>
2016-02-24 20:00:42 -0500

    parse works now

--- Chacha/Chacha/AppDelegate.swift
+++ Chacha/Chacha/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        // Initialize Parse.
+        Parse.setApplicationId("QWzqldyCuWjTaRLRQBBSM3MmhMTgxjCcPnAfp4PV",
+            clientKey: "CxPL3WXPg5pGoWBB6qVb03xSqi6GzcecRBApF0n4")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
         return true
     }
 
