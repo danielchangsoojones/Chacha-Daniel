0f1bafc824f96e9584c93778c9ef82c1c9dd1667
danielchangsoojones <danieljones@Daniels-MBP-3.attlocal.net>
2016-02-24 21:56:08 -0500

    got sign up background working

---
+++ Chacha/Chacha/SignUpViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SignUpViewController.swift
+//  Chacha
+//
+//  Created by Daniel Jones on 2/24/16.
+//  Copyright © 2016 Chong500Productions. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(red: 71.0 / 255, green: 175.0 / 255, blue: 169.0 / 255, alpha: 1.0)
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
