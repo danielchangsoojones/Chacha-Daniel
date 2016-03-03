bef3d0ab41921229a1471ea2cf48c7909f76fa59
danielchangsoojones <danieljones@james-laptop.elysianfields.scottajones.com>
2016-03-03 14:05:33 -0500

    created new personal feed storyboard

---
+++ Chacha/Chacha/PersonalFeedViewController.swift
@@ -0,0 +1,42 @@
+//
+//  PersonalFeedViewController.swift
+//  Chacha
+//
+//  Created by Daniel Jones on 3/3/16.
+//  Copyright © 2016 Chong500Productions. All rights reserved.
+//
+
+import UIKit
+
+class PersonalFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("QuestionCell")! as UITableViewCell
+        
+        cell.textLabel?.text = "test"
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+
+}
